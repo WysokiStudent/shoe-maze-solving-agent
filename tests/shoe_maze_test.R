103284665b2697263ef3f9eafe7280261f636070
Aleksander Bobiński <aleksanderbobinski@interia.eu>
2019-03-01 20:28:28 +0100

    Removed phony test from shoe_maze_test.R

--- tests/shoe_maze_test.R
+++ tests/shoe_maze_test.R
@@ -1,10 +1,5 @@
 source("../src/shoe_maze.R")
 
-test_that("Empty test", {
-
-  expect_equal(1, 1)
-})
-
 test_that("Is maze a matrix", {
   problem <- initialize.problem()
   expect_true(is.matrix(problem$maze))
