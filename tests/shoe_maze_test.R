98989e0c8b098eb6f2e1d519a51c1e616dceee9d
Aleksander Bobiński <aleksanderbobinski@interia.eu>
2019-03-01 20:24:54 +0100

    Added test to check if problem$maze is a matrix

--- tests/shoe_maze_test.R
+++ tests/shoe_maze_test.R
@@ -1,4 +1,11 @@
+source("../src/shoe_maze.R")
+
 test_that("Empty test", {
 
   expect_equal(1, 1)
+})
+
+test_that("Is maze a matrix", {
+  problem <- initialize.problem()
+  expect_true(is.matrix(problem$maze))
 })
