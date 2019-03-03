f5b4c2b4e1b15bb787334da30c2a938ee8099317
Aleksander Bobiński <aleksanderbobinski@interia.eu>
2019-03-03 23:03:06 +0100

    Renamed kRedMaze to kMaze and removed 1 test due to the solution to the problem being reimagined

--- data/tests/problem template test.R
+++ data/tests/problem template test.R
@@ -1,17 +1,12 @@
 library(testthat)
 source("../../problems/problem template.R")
 
-test_that("Is kRedMaze a matrix", {
+test_that("Is kMaze a matrix", {
   problem <- initialize.problem()
-  expect_true(is.matrix(problem$kRedMaze))
+  expect_true(is.matrix(problem$kMaze))
 })
 
-test_that("Is kRedMaze not empty", {
+test_that("Is kMaze not empty", {
   problem <- initialize.problem()
-  expect_false(anyNA(problem$kRedMaze))
-})
-
-test_that("Are kRedMaze states well defined", {
-  problem <- initialize.problem()
-  expect_true(is.logical(problem$kRedMaze))
+  expect_false(anyNA(problem$kMaze))
 })
