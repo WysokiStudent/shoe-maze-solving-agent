68dcec7ce51f745d942dcc12e655c96f0610aa65
Aleksander Bobiński <aleksanderbobinski@interia.eu>
2019-03-09 17:35:07 +0100

    Added test to see if kMaze is well defined

--- data/tests/problem template test.R
+++ data/tests/problem template test.R
@@ -10,3 +10,15 @@ test_that("Is kMaze not empty", {
   problem <- initialize.problem()
   expect_false(anyNA(problem$kMaze))
 })
+
+test_that("Are kMaze cells well defined", {
+  problem <- initialize.problem()
+
+  for (r in nrow(problem$kMaze))
+    for (c in ncol(problem$kMaze)) {
+      expect_true(is.logical(problem$kMaze[r, c][[1]]$isRed))
+      expect_true(is.list(problem$kMaze[r, c][[1]]$walls))
+      for (wall in problem$kMaze[r, c][[1]]$walls)
+        expect_true(is.logical(wall))
+    }
+})
